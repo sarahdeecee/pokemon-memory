b8f9363cb7d02e70d362b64dd01921fa54ddeec2
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-29 20:49:37 -0400

    implement Next pokemon (add to list)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from '@mui/material';
 
 const totalPokemonCount = 1015;
 const capitalize = (string: string): string => {
   return string[0].toUpperCase() + string.slice(1,string.length);
 }
 
+type Pokemon = {name: string, id: number};
+
 function App() {
-  const [pokemon, setPokemon] = useState<string[]>([]);
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
   const [pool, setPool] = useState<string[]>([]);
   const [reset, setReset] = useState<boolean>(false);
+  const [add, setAdd] = useState<boolean>(false);
 
   useEffect(() => {
+    setAdd(false);
     const randomNumber = Math.ceil(Math.random()*totalPokemonCount);
       if (!reset) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`)
           .then(res => res.json())
           .then(
             (result) => {
-              setPokemon([...pokemon, capitalize(result.name)]);
+              setPokemon([...pokemon, {name: capitalize(result.name), id: result.id}]);
               console.log(result.name);
             },
             (error) => {
@@ -26,11 +31,18 @@ function App() {
             }
           )
       }
-  }, [reset])
+  }, [add]);
+
+  const handleAddPokemon = () => {
+    setAdd(true);
+  }
+
+  const pokemonList = pokemon.map(pokemon => <p key={pokemon.id}>{pokemon.name}</p>)
   
   return (
     <div className="App">
-      <p>{pokemon}</p>
+      <p>{pokemonList}</p>
+      <Button onClick={handleAddPokemon}>Next</Button>
     </div>
   );
 }
