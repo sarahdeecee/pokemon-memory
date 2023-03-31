89179ad582660a91cd795ae528b3625507cbbebf
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-31 02:44:24 -0400

    add id, gen to results

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -9,6 +9,7 @@ function Results(props: any) {
       <Typography variant='h3'>{name}</Typography>
       {!empty && button}
       {reveal && <img src={imgSrc} alt={`Illustration of ${name}`} className="Results" hidden={!name} />}
+      {(!empty && reveal) && <Typography variant='h6'>Pokemon #: {id}, Gen {generation}</Typography>}
     </div>
   );
 }
